ea812866ff40b8ded765cc2a303a108dcacc7fb6
Jansen_alves <j.alves01602@gmail.com>
2024-06-14 17:41:57 -0300

    feat: adicionando novos métodos ao controller_fila, modelando services e validator da tabela fila

--- app/controllers/filas_controller.ts
+++ app/controllers/filas_controller.ts
@@ -4,11 +4,44 @@ import { createFilaValidator } from '#validators/fila'
 import { paginationValidator } from '#validators/filter'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
+const Fila = ("app/models/fila");
 
 export default class FilasController {
-    public async store(){
+    constructor(readonly FilaService: FilaService) {}
+
+    public async index({ request, response }: HttpContext) {
+        const search = decodeURI(request.input('search', ''))
+    
+        const pagination = await paginationValidator.validate({
+          page: request.input('page', 1),
+          limit: request.input('limit', 10),
+        })
+    
+        const { page, limit } = pagination
+    
+        const assisteds_fila = await this.FilaService.getAssistedsFila(page, limit, search)
+        return response.json(assisteds_fila)
+      }
+      
+
+      public async show({ response, request }: HttpContext) {
+        const search = request.param('id', '')
+        const assisted_fila = await this.FilaService.getAssistedFila(search)
+    
+    
+        if (!assisted_fila)
+          throw new EntityNotFoundException('O assistido "' + search + '" não foi encontrado!')
+    
+        return response.json(assisted_fila)
+      }
+    
+
+    public async store({request, response}: HttpContext){
+        const data = await createFilaValidator.validate(request.body())
+        const fila = await this.FilaService.createFila(data)
         return {
-            msg:'inserção concluida'
+            msg:'inserção concluida',
+            fila
         }
     }
 }
--- app/services/fila_service.ts
+++ app/services/fila_service.ts
@@ -1,13 +1,77 @@
 import ConflictException from '#exceptions/conflict_exception'
 import Fila from '#models/fila'
+import { CreateFila } from '#validators/fila'
+import { PageResult } from '../utils/pageable.js'
+
+const uuidRegex = /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/
 
 export default class FilaService {
+
+    /**
+   * Busca diversos assistidos pelo nome, nome social ou cpf
+   *
+   * @param page A página atual
+   * @param perPage A quantidade de assistidos por página
+   * @param search O nome, id
+   * @returns
+   */
+
+    async getAssistedsFila(page: number, perPage: number, search: string | null) {
+      const query = Fila.query().preload('assistedID')
+  
+      if (search) {
+        console.log(search)
+        if (uuidRegex.test(search)) {
+          return await query.where('id', search).paginate(page, perPage)
+        }
+  
+        return await query
+          .whereRaw(`name REGEXP '^[${search}]'`)
+          .orWhereRaw(`social_name REGEXP '^[${search}]'`)
+          .paginate(page, perPage)
+      }
+  
+      const assistedsPagination = await query.paginate(page, perPage)
+      const assisteds = assistedsPagination.all()
+  
+      return PageResult.toResult(assisteds, {
+        currentPage: page,
+        itemsPerPage: perPage,
+        totalPages: assistedsPagination.lastPage,
+        totalItems: assistedsPagination.total,
+      })
+    }
+
+    /**
+   * Busca um assistido pelo nome, nome social 
+   *
+   * @param search
+   * @returns
+   */
+
+  async getAssistedFila(search: string) {
+    const query = Fila.query().preload('assistedID')
+
+    if (uuidRegex.test(search)) {
+      return await query.where('id', search).firstOrFail()
+    }
+
+    return query
+      .whereRaw(`name REGEXP '^[${search}]'`)
+      .orWhereRaw(`social_name REGEXP '^[${search}]'`)
+      .firstOrFail()
+  }
+
+  
   /**
-   * Para registrar uma nova droga no banco de dados
+   * Para registrar um novo assistido na fila criada
    *
    * @param name Nome da nova droga
    * @returns
    */
+  async createFila(createFilaTo: CreateFila): Promise<Fila> {
+    return await Fila.create(createFilaTo)
+  }
 
  
 }
